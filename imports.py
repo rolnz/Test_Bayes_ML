856abdec4ca54ef654d2a7c6a078a10cc7fc6524
unknown <nzalabacka.roland@gmail.com>
2023-03-31 19:18:36 +0200

    init

---
+++ imports.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from scipy.linalg 
